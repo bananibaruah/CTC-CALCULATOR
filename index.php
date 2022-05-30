f88ab224dfce22bcb4dcdc77aaadb39b380dbf53
BANANI BARUAH <39000053+bananibaruah@users.noreply.github.com>
2022-05-29 23:26:19 -0700

    done

---
+++ index.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CTC Calculator</title>
+    <link rel="stylesheet" href="css\bootstrap.min.css">
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <br><br><div style="text-align: center;"><b>CTC CALCULATOR</b>
+    <br>
+    <br>
+                    <button class="GFG1" 
+    onclick="window.location.href = 'FTE';" style="float: center;">
+        FTE
+    </button>
+
+    <button class="GFG1" 
+    onclick="window.location.href = 'PE';" style="float: center;">
+        PE
+    </button>
+
+      <!-- <select id="offer_select"> 
+                        <option>Choose Offer Letter</option>
+                        <option value="blue"><a href="FTE">FTE</a></option>
+                        <option value="red" selected><a href="PE">PE E10 to S10</a></option>
+                            <option value="red" selected><a href="">PE (M10 & Above)</a></option>
+                            
+                            <option value="yellow"><a href="">CAMPUS</a></option>
+                            <option value="yellow"><a href="">OFF CAMPUS</a></option>
+                        </select> -->
+
+
+</div>
+</body>
+</html>
