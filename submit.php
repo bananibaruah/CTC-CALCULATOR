5bdfafe86754d8e50fc64343e24792b61ffabc5b
BANANI BARUAH <baruahbanani@gmail.com>
2022-07-25 15:43:03 +0530

    done

--- submit.php
+++ submit.php
@@ -37,8 +37,8 @@ $Ref_No_of_Termination_Letter=$_POST["Ref_No_of_Termination_Letter"];
 $Consultant_Contract_date_Ref=$_POST["Consultant_Contract_date_Ref"];
 $Highest_Qualification=$_POST["Highest_Qualification"];
 $Total_Exp_in_yrs=$_POST["Total_Exp_in_yrs"];
-// $Rel_Exp_in_yrs=$_POST["Rel_Exp_in_yrs"];
-// $Present_Employer=$_POST["Present_Employer"];
+$Rel_Exp_in_yrs=$_POST["Rel_Exp_in_yrs"];
+$Present_Employer=$_POST["Present_Employer"];
 $Delivery_Leader=$_POST["Delivery_Leader"];
 $Delivery_Head=$_POST["Delivery_Head"];
 $Reporting_Manager=$_POST["Reporting_Manager"];
@@ -46,15 +46,15 @@ $Client_Name=$_POST["Client_Name"];
 $Source=$_POST["Source"];
 $Source_Name=$_POST["Source_Name"];
 $Recruiter_Name=$_POST["Recruiter_Name"];
-// $Medical_Status=$_POST["Medical_Status"];
-// $Eligible_for_Relocation_Allowance=$_POST["Eligible_for_Relocation_Allowance"];
+$Medical_Status=$_POST["Medical_Status"];
+$Eligible_for_Relocation_Allowance=$_POST["Eligible_for_Relocation_Allowance"];
 $Relocation_Allowance_Clause=$_POST["Relocation_Allowance_Clause"];
 $Mail_Subject__Offer_Letter_Name=$_POST["Mail_Subject__Offer_Letter_Name"];
 $Adhar_Card_Details=$_POST["Adhar_Card_Details"];
 $Pan_Card_Details=$_POST["Pan_Card_Details"];
-// $Fresher_OLBond=$_POST["Fresher_OLBond"];
-// $Bond_Amount=$_POST["Bond_Amount"];
-// $Bond_Expiry_Date=$_POST["Bond_Expiry_Date"];
+$Fresher_OLBond=$_POST["Fresher_OLBond"];
+$Bond_Amount=$_POST["Bond_Amount"];
+$Bond_Expiry_Date=$_POST["Bond_Expiry_Date"];
 $Offer_Prepared_By=$_POST["Offer_Prepared_By"];
 $Offer_Acceptance=$_POST["Offer_Acceptance"];
 $Offer_Acceptance_Date=$_POST["Offer_Acceptance_Date"];
@@ -100,8 +100,8 @@ $sql = "INSERT INTO `olt`
 `Consultant_Contract_date_Ref`,
 `Highest_Qualification`,
 `Total_Exp_in_yrs`,
--- `Rel_Exp_in_yrs`,
--- `Present_Employer`,
+`Rel_Exp_in_yrs`,
+`Present_Employer`,
 `Delivery_Leader`,
 `Delivery_Head`,
 `Reporting_Manager`,
@@ -109,15 +109,15 @@ $sql = "INSERT INTO `olt`
 `Source`,
 `Source_Name`,
 `Recruiter_Name`,
--- `Medical_Status`,
--- `Eligible_for_Relocation_Allowance`,
+`Medical_Status`,
+`Eligible_for_Relocation_Allowance`,
 `Relocation_Allowance_Clause`,
 `Mail_Subject__Offer_Letter_Name`,
 `Adhar_Card_Details`,
 `Pan_Card_Details`,
--- `Fresher_OLBond`,
--- `Bond_Amount`,
--- `Bond_Expiry_Date`,
+`Fresher_OLBond`,
+`Bond_Amount`,
+`Bond_Expiry_Date`,
 `Offer_Prepared_By`,
 `Offer_Acceptance`,
 `Offer_Acceptance_Date`,
@@ -162,8 +162,8 @@ VALUES
 '$Consultant_Contract_date_Ref',
 '$Highest_Qualification',
 '$Total_Exp_in_yrs',
--- '$Rel_Exp_in_yrs',
--- '$Present_Employer',
+'$Rel_Exp_in_yrs',
+'$Present_Employer',
 '$Delivery_Leader',
 '$Delivery_Head',
 '$Reporting_Manager',
@@ -171,15 +171,15 @@ VALUES
 '$Source',
 '$Source_Name',
 '$Recruiter_Name',
--- '$Medical_Status',
--- '$Eligible_for_Relocation_Allowance',
+'$Medical_Status',
+'$Eligible_for_Relocation_Allowance',
 '$Relocation_Allowance_Clause',
 '$Mail_Subject__Offer_Letter_Name',
 '$Adhar_Card_Details',
 '$Pan_Card_Details',
--- '$Fresher_OLBond',
--- '$Bond_Amount',
--- '$Bond_Expiry_Date',
+'$Fresher_OLBond',
+'$Bond_Amount',
+'$Bond_Expiry_Date',
 '$Offer_Prepared_By',
 '$Offer_Acceptance',
 '$Offer_Acceptance_Date',
@@ -200,7 +200,7 @@ $olt_id = $row['ol'];
     // header("location: ../../FTE/index.php?a=".base64_encode(base64_encode($Position))."&&b=".base64_encode(base64_encode($Employee_Name)).
     // "&&c=".base64_encode(base64_encode($City))."&&ol=".$olt_id."");
 
-    header("location: ../../CTC-CALCULATOR/FTE/index.php?j=".base64_encode(base64_encode($Employee_Name))."&&s=".base64_encode(base64_encode($Ref_No_of_Offer_Letter)).
+    header("location: ../../CTC-CALCULATORN/FTE/index.php?j=".base64_encode(base64_encode($Employee_Name))."&&s=".base64_encode(base64_encode($Ref_No_of_Offer_Letter)).
 "&&u=".base64_encode(base64_encode($Offer_Date))."&&w=".base64_encode(base64_encode($Sd))."&&z=".base64_encode(base64_encode($ed)).
 "&&aa=".base64_encode(base64_encode($Position))."&&bb=".base64_encode(base64_encode($Address_Line_1))."&&cc=".base64_encode(base64_encode($Address_Line_2)).
 "&&dd=".base64_encode(base64_encode($Address_Line_3))."&&ee=".base64_encode(base64_encode($City ))."&&ff=".base64_encode(base64_encode($Pincode))."&&ol=".$olt_id."");
